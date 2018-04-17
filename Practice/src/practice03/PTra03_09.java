3db91c2589eb5fdadb461c230e937559f867d7fa
kanamarumizuki <mizuki.k.1997@gmail.com>
2018-04-17 11:17:32 +0900

    PTra03_09まで完了

--- Practice/src/practice03/PTra03_09.java
+++ Practice/src/practice03/PTra03_09.java
@@ -18,5 +18,39 @@ public class PTra03_09 {
 		 *
 		 * ※ プログラムは何行書いても良いです
 		 */
+		 	System.out.println("０～6までの数字を入力してもらいます");
+
+		 	int num =  new java.util.Scanner(System.in).nextInt();
+
+		 	switch(num)	{
+		 	case 0:
+		 		System.out.println("日曜");
+		 		break;
+		 		case 1:
+		 		System.out.println("月曜");
+		 		break;
+		 		case 2:
+		 		System.out.println("火曜");
+		 		break;
+		 		case 3:
+		 		System.out.println("水曜");
+		 		break;
+		 		case 4:
+		 		System.out.println("木曜");
+		 		break;
+		 		case 5:
+		 		System.out.println("金曜");
+		 		break;
+		 		case 6:
+		 		System.out.println("土曜");
+		 		break;
+		 	default:
+		 		System.out.println("０～６の整数を入力してください");
+		 	}
+
+
+
+
 	}
 }
+
