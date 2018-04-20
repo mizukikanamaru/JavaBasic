89adcd730d16686ad9aef07c25fed45a26f139a4
kanamarumizuki <mizuki.k.1997@gmail.com>
2018-04-20 15:10:07 +0900

    PTra08_02まで完了

--- Practice/src/practice07/PTra07_04.java
+++ Practice/src/practice07/PTra07_04.java
@@ -10,15 +10,18 @@ public class PTra07_04 {
 	public static void main(String[] args) {
 
 		// ★ int型の配列trumpを宣言し、52個の領域を確保してください
-
+		int[] trump = new int[52];
 
 		// ★ 配列trumpに、[1,2,3,4,5,・・・・,52] を代入してください
-
+		for( int i = 0; i < 52; i++) {
+			trump[i] = i + 1;
+		}
 
 		// ★ 0～51の中からランダムで値を取得して、変数indexに代入してください
-
+		int i= new java.util.Random().nextInt(52);
+		int index = i;
 
 		// ★ 配列trumpのindex番目の中に入っている数字を出力してください
-
+		System.out.println(index);
 	}
 }
--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -16,6 +16,6 @@ public class PTra07_06 {
 		};
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
-
+		System.out.println((array[0][0] + array[0][1] + array[0][2] + + array[1][0] + array[1][1] + array[1][2] + array[1][3])/ 7 );
 	}
 }
